751c5e1e079ca207d2dd7be32761d01c6a0356ad
TheDarkTron <darktron1337@gmail.com>
2021-11-03 08:41:26 +0100

    initial commit

---
+++ rm-center-songs.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         RM Center Songs
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Center the song and show panels on RM.FM
+// @author       TheDarkTron
+// @match        https://www.rm.fm/*/feed
+// @icon         https://www.google.com/s2/favicons?domain=rm.fm
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+GM_addStyle ( `
+    rm-track > div {
+        left: 25%;
+        width: 330px;
+        position: relative;
+    }
+    rm-show > div {
+        left: 25%;
+        width: 330px;
+        position: relative;
+    }
+` );
+
