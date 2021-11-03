adefc3b0a2c22cbbb399aa7c96a7209f368ba99d
Dark Tron <darktron1337@gmail.com>
2021-11-03 11:22:22 +0100

    add wide stackoverflow

---
+++ wide-stackoverflow.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Wide StackOverflow
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make StackOverflow great again! Display StackOverflow at 90% widenes.
+// @author       TheDarkTron
+// @license      MIT
+// @match        *://stackoverflow.com/*
+// @match        *://*.stackexchange.com/*
+// @icon         https://www.google.com/s2/favicons?domain=stackoverflow.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector('body > div.container').setAttribute('style', 'max-width: 90%')
+    document.querySelector('#content').setAttribute('style', 'max-width: 90%')
+})();
